bd93d756a5527866fcb8da83b3f7599bb3575d24
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2018-11-07 15:51:47 -0200

    Post new tweet

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import twitterLogo from '../twitter.svg'
+import api from '../services/api'
+import './Timeline.css'
+import axios from 'axios'
 
-// import { Container } from './styles';
+axios.defaults.baseURL = 'http://localhost:3000'
 
 export default class Timeline extends Component {
+  state = {
+    newTweet: ''
+  }
+  handleInputChange = e => {
+    this.setState({ newTweet: e.target.value })
+  }
+  handleNewTweet = async e => {
+    console.log('API', JSON.stringify(api))
+    if (e.keyCode !== 13) return;
+    const content = this.state.newTweet
+    const author = localStorage.getItem('@GoTwitter:username')
+    await api.post('tweets', { content , author })
+    this.setState({ newTweet: '' })
+  }
+
   render() {
-    return <h1>Timeline</h1>;
+    return (
+      <div className='timeline-wrapper'>
+        <img height={24} src={twitterLogo} alt='GoTwitter' />
+        <form>
+          <textarea
+            value={this.state.newTweet}
+            onChange={this.handleInputChange}
+            onKeyDown={this.handleNewTweet}
+            placeholder='O que está acontecendo?'
+          />
+        </form>
+      </div>
+    )
   }
 }
