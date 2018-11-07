abd6bf1967a03bd5dbfd9b82ffdfa486ae468c6d
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2018-11-07 16:25:14 -0200

    Add the socket.io in the Timeline.js

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import twitterLogo from '../twitter.svg'
 import api from '../services/api'
+import socket from 'socket.io-client'
 import './Timeline.css'
+import Tweet from '../components/Tweet'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://localhost:3000'
 
 export default class Timeline extends Component {
   state = {
+    tweets: [],
     newTweet: ''
   }
+
+  async componentDidMount () {
+    this.subscribeToEvents()
+    const response = await api.get('tweets')
+    this.setState({ tweets: response.data })
+  }
+
+  subscribeToEvents = () => {
+    const io = socket('http://localhost:3000')
+    io.on('tweet', data => {
+      this.setState({tweets: [data, ...this.state.tweets]})
+    })
+    io.on('like', data => {
+      this.setState({tweets: this.state.tweets.map(tweet =>
+        tweet._id === data._id ? data : tweet
+      )
+    })
+    })
+  }
+
   handleInputChange = e => {
     this.setState({ newTweet: e.target.value })
   }
@@ -34,6 +57,9 @@ export default class Timeline extends Component {
             placeholder='O que está acontecendo?'
           />
         </form>
+        { this.state.tweets.map(tweet =>
+        <Tweet key={tweet._id} tweet={tweet}  />)
+        }
       </div>
     )
   }
